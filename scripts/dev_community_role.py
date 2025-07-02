8410e6cf2f9e76341b403544b23969b3cd9afbac
Prabesh01 <prabesh01@pm.me>
2025-07-02 13:26:34 +0000

    scripts/dev: dev community lead role sync

--- scripts/dev_community_role.py
+++ scripts/dev_community_role.py
@@ -14,6 +14,8 @@ DEV_COM_SERVER_ID=1379867449050140752
 MOD_ROLE=951811319370248213
 STUDENT_ROLE=1068069232841068544
 DEV_ROLE=951812307133030430
+BIT_DEV_LEAD_ROLE=1317105223566626848
+DEV_COM_LEAD_ROLE=1379871219184308254
 
 @bot.event
 async def on_ready() -> None:
@@ -24,11 +26,17 @@ async def on_ready() -> None:
     student=bit_server.get_role(STUDENT_ROLE)
     dev=bit_server.get_role(DEV_ROLE)
 
+    dev_server_lead=dev_server.get_role(DEV_COM_LEAD_ROLE)
+    bit_server_dev_lead=bit_server.get_role(BIT_DEV_LEAD_ROLE)
+
     devs=[]
+    dev_leads=[]
     async for member in dev_server.fetch_members(limit=None):
         devs.append(member.id)
+        if dev_server_lead in member.roles: dev_leads.append(member.id)
 
     for memb in dev.members:
+        if bit_server_dev_lead in memb.roles and memb.id not in dev_leads: await memb.remove_roles(bit_server_dev_lead)
         if memb.bot: continue
         if not student in memb.roles: continue
         if mod in memb.roles: continue
@@ -37,6 +45,9 @@ async def on_ready() -> None:
     async for member in bit_server.fetch_members(limit=None):
         if member.id in devs:
             try: await member.add_roles(dev)
+            except: pass
+        if member.id in dev_leads:
+            try: await member.add_roles(bit_server_dev_lead)
             except: pass
 
     print("Done")
